0139975c84d4649a0907bd32d5f7276f6db6838a
BlockProgram <blockprogram@protonmail.com>
2020-03-05 10:49:11 +0100

    add functional programming js file

--- _site/js/fatArrow.js
+++ _site/js/fatArrow.js
@@ -1,22 +1,21 @@
 const arr = [1, 2, 3, 4, 5]; 
 
-// const arrPlusOne = arr.map(function(nombre) {  
-//     return `${nombre} + 1 = ${nombre + 1}`; 
+// const arrPlusOne = arr.map(function(number) {  
+//     return `${number} + 1 = ${number + 1}`; 
 // });
 
-// const arrPlusOne = arr.map((nombre) => {  
-//     return `${nombre} + 1 = ${nombre + 1}`; 
+// const arrPlusOne = arr.map((number) => {  
+//     return `${number} + 1 = ${number + 1}`; 
 // });
 
-// const arrPlusOne = arr.map(nombre => {  
-//     return `${nombre} + 1 = ${nombre + 1}`; 
+// const arrPlusOne = arr.map(number => {  
+//     return `${number} + 1 = ${number + 1}`; 
 // });
 
-const arrPlusOne = arr.map(nombre => `${nombre} + 1 = ${nombre + 1}`);
+const arrPlusOne = arr.map(number => `${number} + 1 = ${number + 1}`);
 
 
 // When no arguments, here is the shortest version 
 const arrPlusOne = arr.map(() => `5`); 
 
-
-console.log(arrPlusOne)
+console.log(arrPlusOne); 
--- _site/js/main.js
+++ _site/js/main.js
@@ -1,34 +1,9 @@
-let b         = [2, 5, 54, 493]; 
-const divHtml = document.querySelector(".calc-wrapper"); 
-const firstP  = document.createElement("p"); 
-const secondP = document.createElement("p"); 
-const thirdP  = document.createElement("p"); 
+const input = document.querySelector("input"); 
 
+input.addEventListener("focus", function() {  
+    input.style.backgroundColor = "pink"; 
+}); 
 
-function mapIt(element) {  
-   return element.map(number => number * number); 
-} 
-divHtml.appendChild(firstP); 
-firstP.innerHTML = mapIt(b);
-
-
- function filterIt(element) {  
-     return element.filter(number => number * number === 25); 
- } 
-
- divHtml.appendChild(secondP); 
-secondP.innerHTML = filterIt(b);
-
-
- function reduceIt(element) {  
-     return element.reduce((total, n) => total += n)
- } 
-
- divHtml.appendChild(thirdP); 
-thirdP.innerHTML = reduceIt(b);
-
-
-let phrase = "honey badger".split("");
-for (let i = 0; i < phrase.length; i++) { 
-    console.log(phrase[i]); 
- } 
+input.addEventListener("blur", function() {  
+    input.style.backgroundColor = "#fff"; 
+}); 
